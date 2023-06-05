923d46f94b0e80ad59dcff5197f8bf6bc82c7e91
Glauco <glauco.pater@gmail.com>
2023-06-05 08:40:24 +0200

    Update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page !</h1>
+      <h1>Welcome to the Dashboard</h1>
       <Link to="/reminders">Go to Reminders</Link>
     </div>
   );
